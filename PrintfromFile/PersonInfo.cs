dc9a4f2442fbafdfd2fde281169c5f9a19591659
Juandi-M <juanmonge95@gmail.com>
2018-08-20 14:17:17 -0600

    1st commit, read and store info from txt to properrties

---
+++ PrintfromFile/PersonInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintfromFile
+{
+    public class PersonInfo
+    {
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+        public string SocialSecurity { get; set; }
+        public string Residence { get; set; }
+
+       
+    }
+}
--- PrintfromFile/Program.cs
+++ PrintfromFile/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PrintfromFile;
 
 namespace PrintfromFile
 {
@@ -10,6 +12,52 @@ namespace PrintfromFile
     {
         static void Main(string[] args)
         {
+            ReadData();
+        }
+        
+
+        public static void ReadData()
+        {
+            List<PersonInfo> persondata = new List<PersonInfo>();
+
+
+            using (StreamReader sr =
+                new StreamReader(path: "C:\\Users\\Juan Monge\\source\\repos\\PrintfromFile\\TESTPRACTICE.txt"))
+            {
+                if (sr != null)
+                {
+                    while (sr.Peek() >= 0)
+                    {
+                        string str;
+                        string[] strArray;
+                        str = sr.ReadLine();
+
+                        strArray = str.Split(',');
+                        PersonInfo currentPerson = new PersonInfo();
+
+                        currentPerson.Name = strArray[0];
+                        currentPerson.Lastname = strArray[1];
+                        currentPerson.Age = Convert.ToInt32(strArray[2]);
+                        currentPerson.SocialSecurity = (strArray[3]);
+                        currentPerson.Residence = (strArray[4]);
+
+                        persondata.Add(currentPerson);
+                        Console.WriteLine(currentPerson);
+                        Console.ReadKey();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("no textFile t");
+                }
+
+            }
+
+        }
+
+        public static void WriteData()
+        {
+
         }
     }
 }
